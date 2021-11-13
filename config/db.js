f316123e298c115bff1688023a96756591dad0e2
Starskii <jakeylakie@yahoo.com>
2021-11-13 18:05:55 -0500

    Connected to MongoDB

---
+++ config/db.js
@@ -0,0 +1,18 @@
+//Connect to MongoDB
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(db, {
+            useNewUrlParser: true
+        });
+        console.log('MongoDB Connected...');
+    } catch(err){
+        console.errer(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
